7a85d6318a6a1d8434aa35d69f79ecc1da03e050
binarypsilocybin <rogeriosvaldo@gmail.com>
2023-02-07 14:55:22 +0000

    new one

--- src/App.js
+++ src/App.js
@@ -2,8 +2,34 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
 
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: green;
+  color: white;
+`;
+
+const Container = styled.div`
+  background: red;
+  color: white;
+  .hero {
+    font-size: 8rem;
+  }
+`;
+
 function App() {
-  return <h4>comfy sloth starter</h4>;
+  return (
+    <div>
+      <h4>comfy sloth starter</h4>
+      <Button>click me</Button>
+      <Container>
+        <div>
+          <h3>Hello World</h3>
+        </div>
+        <div className="hero">hero text</div>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
